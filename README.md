ef07bed838c82c11c096037071b46d2c3d67432e
Brian D. Caruso <bdc34@cornell.edu>
2019-02-12 16:37:03 -0500

    Removed dependency on ewmctrl.el
    Copied some functions from ewmctrl.el to helm-taskswitch.
    Set required helm version to 3.0

--- README.md
+++ README.md
@@ -7,14 +7,25 @@ that problem.
 
 ## How I use this
 
-I map win-a to be globally handled by the window manager to run
-``` emacsclient -n -e '(helm-taskswitch)' ```
+### Hot key to activate helm-taskswitch
+I map the win-a key to be globally handled by the window manager to
+run ``` emacsclient -n -e '(helm-taskswitch)' ```
+
+### Force all browser tabs to a window instead
+A lot of tasks are done in browser tabs. But they are not accessable
+to task switchers. I definitly need to switch between browser tabs to
+get my work done.
 
 To get chrome browser tabs exposed to the helm-taskswitcher, I use a
 extension called 'New Tab, New Window' so each tab is in its own
 window. To further assist with searching for tabs I use an extension
-'URL in title'
+'URL in title'.
 
+These would allow access to tabs from any task switcher but most
+mainstream ones would be useless with dozens of browser windows. Using
+matching from helm alows helm-taskswitcher to quickly find them.
+
+### Example 
 With this setup, when I hit win-a, Emacs comes up and I get the task
 switcher, from there I type in the first thing that comes to mind
 related to the tool I want, the title, the URL or host, .js to get all
@@ -22,13 +33,20 @@ my JavaScript buffers. Helm handles all the auto-complete and
 filtering. Hitting return brings the window/tab/buffer to the
 foreground.
 
+Lets say I want a chrome window with the an AWS ElasticSearch console.
+
+I hit win-a, type 'go' (that filers to all google chrome windows)
+SPACE 'ela' (all google chrome with 'ela' in title) SPACE 'con' (all
+chrome with 'ela' and 'con' in title). Then I usually just hit return
+without looking since I'm confident it will be right.
+
 This works great for me. I still use alt-tab for most recently used
 program. And if I have my hand on the mouse I use it for task
 switching. 
 
 ## Why?
 
-When working on a computer I'm working on tasks, usually each task is
+When working on a computer I'm working on tasks. Usually each task is
 serviced by a program that is running, a tab in a browser or a buffer
 in Emacs.
 
@@ -48,23 +66,26 @@ need to do that task to a state where I can use it.
 
 Mainstream UIs for switching between tasks almost always use a visual
 UI to offer the options of which running programs can be switched
-between. This does not work with my step 2. The main alternative is a
-heap sorted by most recently used programs (alt-tab etc). 
+between. This does not work with my step 2 since I'm thinking of
+something like "google chrome with AWS lambda console". I don't think
+of how it looks, I don't think of how many times back in a stack that
+might be. 
 
 I find these deficient because I usually have a large number of
-programs/tabs/buffers running, each servicing a task. Mainstream UIs
-don't service tabs/buffers so they don't cover all of the things I
-want to bring forward. They focus on a visual search of open program
-windows. As the number of windows grows these become illegible. Web
-browser tabs are a huge difficulty. If I have three browser windows
-open each with a dozen tabs there is no way I'm going to find the tab
-I want efficiently.
+programs/tabs/buffers running, each servicing a task. They focus on a
+visual search of open program windows. As the number of windows grows
+these become illegible. 
+
+Web browser tabs are a huge difficulty. If I have three browser
+windows open each with a dozen tabs there is no way I'm going to find
+the tab I want efficiently.  Mainstream UIs don't service tabs/buffers
+so they don't cover all of the things I want to bring forward.
 
 The setup I use with this package achieves: 
 
-1) Symbolic searching for programs/tabs/buffers that service tasks. I
-just kit win-a and type something. I touch type so this works well for
-me. I'm never rummaging around. 
+1) Symbolic searching of title, program etc for programs/tabs/buffers
+that service tasks. I just hit win-a and type something. I touch type
+so this works well for me. I'm never rummaging around.
 
 2) Combines the program window search space, tab search space and
 Emacs buffer search space. These are the most common types of tools
@@ -74,3 +95,11 @@ I'm looking for.
 tabs are exposed.
 
 
+## TODO 
+This doesn't keep track of how frequently windows are visited or the
+most recently visited window. I'm working on that. I have the input
+data by listening to xevents. 
+
+## Requirements
+
+Xwindows, wmctrl, emacs, helm. I run this on linux.
--- helm-taskswitch.el
+++ helm-taskswitch.el
@@ -5,12 +5,12 @@
 ;; Created: 2016-05-27
 ;; URL: https://github.com/bdc34/helm-taskswitch
 ;; Package-Version: 20160728.001
-;; Package-Requires: ((helm "20160420.259") (ewmctrl "20150630.138"))
+;; Package-Requires: ((helm "3.0"))
 ;; Keywords: desktop, windows
 
 ;;; Commentary:
 
-;; `helm-taskswitch' provides an window and buffer switching with helm.
+;; `helm-taskswitch' provides an X window and buffer switching with helm.
 
 ;; ## Table of Contents
 
@@ -18,10 +18,11 @@
 ;; - [Usage](#usage)
 ;; - [Issues](#issues)
 ;; - [License](#license)
+;; - [TODOs](#TODOs)
 
 ;; ## Installation
 
-;; TODO Install wmctrl, ewmctrl.el and helm.
+;; Install wmctrl and helm.
 
 ;; ## Usage
 
@@ -37,11 +38,23 @@
 
 ;; * which version of Emacs you're running on which operating system, and
 
-;; * how you installed `helm-task-switcher', wmctrl, helm and ewmctrl.el.
+;; * how you installed `helm-task-switcher', wmctrl and helm.
+
+;; ## TODOs
+
+;; * Track or get with focus history and use it to order candidates. Current
+;;   order is arbitrary.
+
+;; Intersting blog post about alt-tab, suggests markov model
+;; http://www.azarask.in/blog/post/solving-the-alt-tab-problem/
+;; Ideas from that: other hot key for "go back to most recent window"
+
+;; * Keep emacs filter emacs out of focus history when it is used for switching
+
 
 ;; ## License
 
-;; [GNU General Public License version 3](http://www.gnu.org/licenses/gpl.html), or (at your option) any later version.
+;; [GNU General Public License version 3](http://www.gnu.org/licenses/gpl.html), or (at your option) any later version;; Code from https://github.com/flexibeast/ewmctrl is used under GNU 3.
 
 ;;; Code:
 
@@ -51,6 +64,12 @@
 (defgroup helm-taskswitch nil
   "Use helm to switch between tasks (X11 windows or buffers)"
   :group 'external)
+
+(defcustom helm-taskswitch-wmctrl-path "/usr/bin/wmctrl"
+  "Absolute path to wmctrl executable."
+  :type '(file :must-match t)
+  :group 'helm-taskswitch
+) 
 
 (defgroup helm-taskswitch-faces nil
   "Customize the appearance of helm-taskswitch."
@@ -75,36 +94,41 @@
   :group 'helm-taskswitch-faces)
 
 
-;; Things that would be nice to have customizations for:
-;; boring client windows
-;; faces
-;; truncate long lines
+(defcustom helm-taskswitch-pre-switch-hook '()
+  "Called before a switch to a client, not called in other helm sources, buffers, etc.
+
+Each Function is called with two arguments, 
+First the x window id.
+Second, the candidate window associative list."
+  :type '(repete function)
+  :group 'helm-taskswitch)
 
 ;; Internal Code
 
 (defun helm-taskswitch-format-candidate (al)
-  "Formats a candidate client window for task switcher"
-  (cons (helm-taskswitch-format-candidate-title al) (list al)))
+  "Formats a candidate client window for task switcher.
 
-(defun helm-taskswitch-format-candidate-title (al)
-  "Format the title of the candidate window"
-  (let ((wmclass (helm-taskswitch-wmclass al )) ;(cdr (assoc 'wmclass al))))
-        (title   (helm-taskswitch-title al ))) ;(cdr (assoc 'title   al)))))
-    (format "%-18s  %s" wmclass title )))
+  AL is the associative list for a window."
+  (cons
+   (format "%-18s  %s" (helm-taskswitch-wmclass al) (helm-taskswitch-title al))
+   (list al)))
 
 (defun helm-taskswitch-title (al)
+  "Formats the title of a window.
+
+  AL is an associative list for a window."
   (let* ( (title   (cdr (assoc 'title   al)))
-          (wmclass (cdr (assoc 'wmclass al))) )
-    (cond ((string-equal wmclass "google-chrome.google-chrome" )
-           ;; chrome sometimes ends with "- Google Chrome"
-           ;; and some pages repeat the URL in the title
-           (replace-regexp-in-string "- Google Chrome" "" title))     
-          (t title)) ))
+          (wmclass (cdr (assoc 'wmclass al))))
+     ;; redundent
+    (replace-regexp-in-string "- Google Chrome" ""
+                              ;; bad title for cornell's Jira
+                              (replace-regexp-in-string "- Cornell University Library https://culibrary.atlassian.net.*" "Jira Issues" title))))
+
 
 (defun helm-taskswitch-wmclass (al)
   (let ((title   (cdr (assoc 'title   al)))
-         (wmclass (cdr (assoc 'wmclass al))))
-    (cond ((string-equal wmclass "google-chrome.google-chrome" )
+        (wmclass (cdr (assoc 'wmclass al))))
+    (cond ((string-equal wmclass "google-chrome.Google-chrome" )
            (propertize "Google-chrome" 'face 'helm-taskswitch-browser-face ))
           ((string-equal wmclass "terminator.Terminator")
            (propertize "Terminator" 'face 'helm-taskswitch-term-face))
@@ -112,70 +136,94 @@
            (propertize "Emacs" 'face 'helm-taskswitch-emacs-face))
           (t wmclass) )))
 
+(setq helm-taskswitch--wmctrl-field-count 10)
 
-(defun helm-taskswitch-client-candidates ()
-  "Return a list windows with title and wmclass"
-  (mapcar 'helm-taskswitch-format-candidate (ewmctrl--list-windows)) )
+(defun helm-taskswitch--list-windows ()
+  "Internal function to get a list of desktop windows via `wmctrl'."
+  (let ((bfr (generate-new-buffer " *helm-taskswitch-wmctrl-output*"))
+        (fields-re (concat "^"
+                           (mapconcat 'identity (make-list (1- helm-taskswitch--wmctrl-field-count) "\\(\\S-+\\)\\s-+") "")
+                           "\\(.+\\)"))
+        (windows-list '()))
+    (call-process-shell-command (concat helm-taskswitch-wmctrl-path " -lGxp") nil bfr)
+    (with-current-buffer bfr
+      (goto-char (point-min))
+      (while (re-search-forward fields-re nil t)
+        (let ((window-id (match-string 1))
+              (desktop-number (match-string 2))
+              (pid (match-string 3))
+              (x-offset (match-string 4))
+              (y-offset (match-string 5))
+              (width (match-string 6))
+              (height (match-string 7))
+              (wmclass (match-string 8))
+              (client-host (match-string 9))
+              (title (match-string 10)))
+          (setq windows-list
+                (append windows-list
+                        (list
+                         `((window-id . ,window-id)
+                           (desktop-number . ,desktop-number)
+                           (pid . ,pid)
+                           (x-offset . ,x-offset)
+                           (y-offset . ,y-offset)
+                           (width . ,width)
+                           (height . ,height)
+                           (wmclass . ,wmclass)
+                           (client-host . ,client-host)
+                           (title . ,title))))))))
+    (kill-buffer bfr)
+    windows-list))
 
+
+
+(defun helm-taskswitch-focus-window-by-cancidate (c)
+  "Internal function to focus the desktop window specified by ID."
+  (let ((id (cdr (assoc 'window-id (car c)))))
+    (dolist (f helm-taskswitch-pre-switch-hook) (funcall f c))
+    (call-process-shell-command (concat helm-taskswitch-wmctrl-path " -i -a '" id "'"))))
+  
 (defun helm-taskswitch-close-candidates (c)
-  "Closes a candidate window from ewmctrl--list-windows"
+  "Closes a candidate window from helm-taskswitch--list-windows"
   (mapc (lambda (c)
           (let* ((id (cdr (assoc 'window-id (car c))))
-                 (killcmd (concat ewmctrl-wmctrl-path " -i -c '" id "'")))
+                 (killcmd (concat helm-taskswitch-wmctrl-path " -i -c '" id "'")))
             (message killcmd)
             (call-process-shell-command killcmd )))
         (helm-marked-candidates)))
 
-(defun helm-taskswitch-open-preview-jpg (id)
-  "Make a preview jpg of a x window in /tmp/id.jpg"
 
-  ;; This is too slow and buggy.
-  
-  ;; Encountering some bug in xwd a lot, it looks like this
-  ;;xwd: Getting target window information.
-  ;;
-  ;;X Error of failed request:  BadMatch (invalid parameter attributes)
-  ;;  Major opcode of failed request:  73 (X_GetImage)
-  ;;  Serial number of failed request:  255
-  ;;  Current serial number in output stream:  255
+(defun helm-taskswitch-client-candidates ()
+  "Return a list windows with title and wmclass."
+  (mapcar 'helm-taskswitch-format-candidate (helm-taskswitch--list-windows)) )
 
- (let* ((tmp-win-file (concat "/tmp/" id ".jpg"))
-        (cmd (concat "/usr/bin/xwd" " -silent -id '" id "' | /usr/bin/convert - " tmp-win-file )))
-      (progn
-       (message cmd)
-       (call-process-shell-command cmd)
-       (find-file tmp-win-file))))
-
-(defun helm-taskswitch-make-preview-windows (wmctrl-list)
-   "Make a directory of preview jpegs"
-   (mapcar 'make-preview-jpg wmctrl-list))
-
-;; The class for sources is defined in helm-source.el
-(setq helm-source-wmctrl-windows
-      '((name . "client windows")
-        (candidates . helm-taskswitch-client-candidates)
-        (action . (("Forground" . (lambda (c) (ewmctrl--focus-window-by-id (cdr (assoc 'window-id (car c))))))
+(setq  helm-source-wmctrl-windows
+       (helm-build-sync-source
+           "client windows sync"
+         :fuzzy-match t
+         :candidates 'helm-taskswitch-client-candidates
+         :action '(("Forground" . helm-taskswitch-focus-window-by-cancidate )
                    ("close window" . helm-taskswitch-close-candidates )
                    ("Split top bottom" . (lambda (candidate) (message "Not yet implemented: top-bottom %s" candidate)))
                    ("Split right left" . (lambda (candidate) (message "Not yet implemented: right-left %s" candidate)))
-                   ("preview" . (lambda (c) ( helm-taskswitch-open-preview-jpg (cdr (assoc 'window-id (car c))))))
-                   ("dump client window s-exp" . prin1 )
-                   ))
-        ))
-
-; '(helm-source-buffers-list
-;   helm-source-recentf
-;   helm-source-buffer-not-found)
+                   ;;("preview" . (lambda (c) ( helm-taskswitch-open-preview-jpg (cdr (assoc 'window-id (car c))))))
+                   ("dump client window s-exp" . prin1 ))))
 
 ;;;###autoload
 (defun helm-taskswitch ()
   "Use helm to switch between tasks (X11 windows, buffers or recentf)"
   (interactive)
-  (require 'ewmctrl)
+  (run-hooks 'helm-taskswitch-open-hooks )
   (select-frame-set-input-focus (window-frame (selected-window)))
+
+
+  
   (unless helm-source-buffers-list
-    (setq helm-source-buffers-list
+   (setq helm-source-buffers-list
           (helm-make-source "Buffers" 'helm-source-buffers)))
+
+;; consider :resume t
+;           :preselect 
   (helm :sources '(helm-source-wmctrl-windows
                    helm-source-buffers-list
                    helm-source-recentf 
@@ -184,6 +232,114 @@
         ))
 
 (provide 'helm-taskswitch)
+
+;;-------------------------------------------------------------------------
+;; In progress:  of ordering of candidates 
+
+;; TODO Need focus history before emacs is focused for taskswitch
+;; get this with a process that runs xprop -root -spy
+;; and watches for _NET_ACTIVE_WINDOW
+
+;; TODO sort candidates based history
+;; Get most recent window before task switch, look at out freqs, order by that,
+;; then, for all remain windows, order by global in freq
+;; (let* ((all (get-all-windows))
+;;        (current (get-current-window))
+;;        (out-list (get-out-list current))
+;;        (never-out ( list-subtract all out-list))
+;;        (candidate (append out-list (order-like never-out global-in-freq))))  
+;;   candidate)
+
+;; ( in-freqs * per-win-out-freqs * current_win_id )
+;;
+;; freq-req = ( winid , (times...))
+;;
+;; in-freqs = tree< freq-req >
+;;
+;; per-win-out-freqs = tree< (winid, tree< freq-rec >)>
+
+;; for xproc -spy
+;;   on _NET_ACTIVE_WINDOW: update-per-win-out-freq( current_win_id, new_win_id)
+;;                          current_win_id = new_win_id
+;;   
+;;   
+;; TODO avoid recording emacs in history when used as task switcher
+;; Cannot just remove the emacs switch sicne emacs might have been focused
+;; before the call to helm-taskswitch. Maybe check before the call to
+;; select-frame-set-input-focus?
+;; something like: if not emacs-focused-p: bring-forward-but-remove-from-history
+
+
+;  First stab at how to do this: 1 in emacs use a
+;; process to run xproc -root -spy to listen for focus change events 2
+;;
+;; (defun helm-taskswitch--track-history (proc string)
+;;   ... keep track of focus history ... )
+;; 
+;; (make-process :command '('xproc' '-root' '-spy')
+;;    :connection-type 'pipe'
+;;    :filter helm-taskswitch--track-history) 
+
+
+(require 's)
+
+(defun to-wids  (string)
+  "For a string, find all events that look like a focus change and return
+   a list of the window ids in order"
+
+  ;; interesting events:
+  ;; NET_ACTIVE_WINDOW - newly active/focused window
+  ;; NET_CLIENT_LIST - all client windows
+  (let ((foc-regex  "_NET_ACTIVE_WINDOW(WINDOW): window id # 0x\\([0-9a-f][0-9a-f]+\\)"))
+    (mapcar 'cadr (s-match-strings-all foc-regex string))))
+
+;; 
+
+(defun ordinary-insertion-filter (proc string)
+  (when (buffer-live-p (process-buffer proc))
+    (with-current-buffer (process-buffer proc)
+      (let ((moving (= (point) (process-mark proc)))
+            (lines (to-wids string )))
+        (save-excursion
+          ;; Insert the text, advancing the process marker.
+          (goto-char (process-mark proc))
+          (mapc (lambda (s) (insert (format "%s\n" s))) lines)
+          (set-marker (process-mark proc) (point)))
+        (if moving (goto-char (process-mark proc)))))))
+
+ (make-process
+  :name "xproc-focus-spy"
+  :buffer "*xproc-focus-spy*"
+  :command '("xprop" "-root" "-spy")
+  :noquery t
+  :filter 'ordinary-insertion-filter
+  )
+
+
+;; (defun helm-taskswitch-open-preview-jpg (id)
+;;   "Make a preview jpg of a x window in /tmp/id.jpg"
+
+;;   ;; This is too slow and buggy.
+  
+;;   ;; Encountering some bug in xwd a lot, it looks like this
+;;   ;;xwd: Getting target window information.
+;;   ;;
+;;   ;;X Error of failed request:  BadMatch (invalid parameter attributes)
+;;   ;;  Major opcode of failed request:  73 (X_GetImage)
+;;   ;;  Serial number of failed request:  255
+;;   ;;  Current serial number in output stream:  255
+
+;;  (let* ((tmp-win-file (concat "/tmp/" id ".jpg"))
+;;         (cmd (concat "/usr/bin/xwd" " -silent -id '" id "' | /usr/bin/convert - " tmp-win-file )))
+;;       (progn
+;;        (message cmd)
+;;        (call-process-shell-command cmd)
+;;        (find-file tmp-win-file))))
+
+;; (defun helm-taskswitch-make-preview-windows (wmctrl-list)
+;;    "Make a directory of preview jpegs"
+;;    (mapcar 'make-preview-jpg wmctrl-list))
+
 
 ;;
 ;; This file is NOT part of GNU Emacs.
